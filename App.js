04d41b9aff0fc3ed13332fb85114b6f48e88c74e
obertti.thiago.epet20@gmail.com <Obertti.thiago.epet20@gmail.com>
2024-09-19 12:09:28 -0300

    Importando y conectando botones

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
-import LoginScreen from '../boletin/screens/Login';
+import LoginScreen from '../boletin/screens/Login.jsx';
+import Archivo from './screens/Archivo.jsx';
+import Singin from './singin.jsx';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Singinprofesor from './singin2.jsx';
 
+const Stack = createStackNavigator();
 const App = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar barStyle="dark-content" />
-      <LoginScreen />
-    </SafeAreaView>
-  );
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Boletin Virtual">
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Archivo" component={Archivo} />
+      <Stack.Screen name="Singin" component={Singin} />
+      <Stack.Screen name="Singinprofesor" component={Singinprofesor} />
+
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 };
 
 export default App;
