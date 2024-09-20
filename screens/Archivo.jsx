24d16c64515ebd6db04b50f9e53f17bed8635a15
santy186 <vargassanti186@gmail.com>
2024-09-19 23:04:51 -0300

    Imports Navigation

--- screens/Archivo.jsx
+++ screens/Archivo.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { View, Text,Pressable, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';  
+import { useNavigation } from '@react-navigation/native';
+import Empezar from './Empezar';
 
 const ArchivoScreen = () => {
 const Archivo = () => {
