99a6ca6eb94f95e05e963c6eef480f846fb11cd3
santy186 <vargassanti186@gmail.com>
2024-09-19 22:59:30 -0300

    Pantalla Empezar

---
+++ screens/Empezar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+const Empezar = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Tu Boletín</Text>
+      <Text style={styles.texto}>Ya puedes ver tu información académica!</Text>
+
+      
+    </View>
+  );
+};
