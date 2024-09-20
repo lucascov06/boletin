64c495f22dfb066fa6757164b7021a92b3c0983a
santy186 <vargassanti186@gmail.com>
2024-09-19 23:00:03 -0300

    Image de fondo

--- screens/Empezar.jsx
+++ screens/Empezar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+
 const Empezar = () => {
   return (
     <View style={styles.container}>
+      <Image source={require('../assets/fondo-pantalla-empezar.png')} style={styles.image} />
       <Text style={styles.titulo}>Tu Boletín</Text>
       <Text style={styles.texto}>Ya puedes ver tu información académica!</Text>
 
