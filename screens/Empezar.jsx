97cc0c663da894c2c62e7d3855bbcfebb05daba0
santy186 <vargassanti186@gmail.com>
2024-09-21 13:54:49 -0300

    Arreglos boton

--- screens/Empezar.jsx
+++ screens/Empezar.jsx
@@ -5,55 +5,56 @@ const Empezar = () => {
   return (
     <View style={styles.container}>
       <Image source={require('../assets/fondo-pantalla-empezar.png')} style={styles.image} />
-      <Text style={styles.titulo}>Tu Boletín</Text>
-      <Text style={styles.texto}>Ya puedes ver tu información académica!</Text>
-
-      
-      <TouchableOpacity style={styles.boton}>
-        <Text style={styles.textoBoton}>Empezar</Text>
-      </TouchableOpacity>
+      <View style={styles.overlay}>
+       
+        <Text style={styles.texto}>¡Ya puedes ver tu información académica!</Text>
+        <TouchableOpacity style={styles.boton}>
+          <Text style={styles.textoBoton}>Empezar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#f0f0f0', // Color de fondo
-    },
-    image: {
-      flex: 1,
-      width: null,
-      height: null,
-      resizeMode: 'cover',
-    },
-    titulo: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginTop: 20,
-    },
-    texto: {
-      fontSize: 18,
-      textAlign: 'center',
-      marginTop: 10,
-    },
-    boton: {
-      backgroundColor: '#E95F8B',
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderRadius: 5,
-      width: '10%',
-      alignItems: 'center',
-      marginBottom: 15,
-    },
-    textoBoton: {
-      color: 'white',
-      fontWeight: 'bold',
-    },
-  });
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  overlay: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+ 
+  texto: {
+    fontSize: 25,
+    color: '#fff', // Texto en blanco para mejor contraste
+    textAlign: 'center',
+    marginBottom: 20,
+    marginTop: 100, 
+  },
+  boton: {
+    backgroundColor: '#E95F8B',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+    width: '80%',
+    marginTop: 100, // Ajusta este valor para mover el botón más abajo
+  },
+  
+  textoBoton: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
 
 export default Empezar;
+
