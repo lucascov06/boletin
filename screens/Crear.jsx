8bae2ba98270d6d0f29159f21bdd65ecaad3fd82
santy186 <vargassanti186@gmail.com>
2024-09-22 02:30:57 -0300

    Screen Crear e imports

---
+++ screens/Crear.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import { View, Text,Pressable, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';  
+import { useNavigation } from '@react-navigation/native';
+import Empezar from './Empezar';
+
