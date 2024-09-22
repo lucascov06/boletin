d1bffbb78dea7974d9e869fd3edfe3a8d0854ce7
santy186 <vargassanti186@gmail.com>
2024-09-22 02:35:52 -0300

    Navigation Crear y Contraseña

--- navigation.js
+++ navigation.js
@@ -4,9 +4,11 @@ import Archivo from "./screens/Archivo";
 import { createStackNavigator } from '@react-navigation/stack';
 // screens
 import Singin from "./singin";
-import Singinprofesor from "./screens/singin";
+import Singinprofesor from "./singin2";
 import Empezar from "./screens/Empezar";
 import Contraseña from "./screens/Contraseña"; 
+import Crear from "./screens/Crear"; 
+
 
 const Stack = createStackNavigator();
 
@@ -16,6 +18,8 @@ function Mytabs() {
             <tab.Screen name="Archivo" component={Archivo} />
             <tab.Screen name="Singin" component={Singin} />
             <tab.Screen name="Empezar" component={Empezar} />
+            <tab.Screen name="Crear" component={Crear} />
+            <tab.Screen name="Contraseña" component={Contraseña} />
             <tab.Screen name="Singinprofesor" component={Singinprofesor } />
 
         </Stack.Navigator>    
