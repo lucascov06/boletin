d6837468ff9c69ff3a550b53868519e53959e94f
Emanuel <arielruizcontacto@gmail.com>
2024-10-29 17:43:39 -0300

    Agregando informacion a la presentacion

--- screens/presentacion.jsx
+++ screens/presentacion.jsx
@@ -4,5 +4,7 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 
 
 cons
-const InfoScreen = () => 
-const handlePress = () => 
+const InfoScreen = () =>{}
+const handlePress = () => {}
+
+<Text style={styles.title}>Ya puedes ver tu informacion academica.</Text>
