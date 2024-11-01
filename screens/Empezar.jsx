fe6b4da8dce7b22e6141b6eacd3b5ab1376d7bf1
obertti.thiago.epet20@gmail.com <Obertti.thiago.epet20@gmail.com>
2024-10-31 21:19:02 -0300

    importando home y useNavigation

--- screens/Empezar.jsx
+++ screens/Empezar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet,Pressable, TouchableOpacity } from 'react-native';
 
+import { useNavigation } from '@react-navigation/native';
+import HomeScreen from './Home';
 const Empezar = () => {
   return (
     <View style={styles.container}>
