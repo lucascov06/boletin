63e8ac957ad572256babf79fc21102444480bcba
obertti.thiago.epet20@gmail.com <Obertti.thiago.epet20@gmail.com>
2024-10-31 21:20:30 -0300

    Importando para crear la pantalla Home

---
+++ screens/Home.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, ScrollView, StyleSheet,Pressable } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
