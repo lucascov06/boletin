04412f9ee6296b003b6050dcb3954dece9f05984
obertti.thiago.epet20@gmail.com <Obertti.thiago.epet20@gmail.com>
2024-11-05 13:41:08 -0300

    creando pantalla home y dando estilos

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { View, Text, Image, TouchableOpacity, ScrollView, StyleSheet,Pressable } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-
 function HomeScreen() {
 return (
     <View style={styles.container}>
@@ -22,7 +21,7 @@ return (
     </View>
 
     <ScrollView horizontal style={styles.categories}>
-        {['Primer Trimestre', 'Segundo Trimestre', 'Tercer Trimestre', 'Profesores', 'Escuela'].map((item, index) => (
+        {['Tercer Trimestre', 'Profesores', 'Escuela'].map((item, index) => ( // Solo hacemos el ultimo trimestre
         <View key={index} style={styles.category}>
             <Text>{item}</Text>
         </View>
@@ -52,23 +51,91 @@ return (
 }
 
 const styles = StyleSheet.create({
-container: { flex: 1, padding: 16, backgroundColor: '#fff' },
-    header: { flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', padding: 16 },
-    headerTitle: { fontSize: 20, fontWeight: 'bold' },
-    profileImage: { width: 32, height: 32, borderRadius: 16 },
-    searchFilterContainer: { flexDirection: 'row', justifyContent: 'space-between', marginVertical: 16 },
-    searchText: { fontSize: 24, fontWeight: 'bold' },
-    sortFilter: { flexDirection: 'row' },
-    icon: { marginHorizontal: 8 },
-    categories: { flexDirection: 'row', marginBottom: 14, width: 150, },
-    category: { marginHorizontal: 8, padding: 8, backgroundColor: '#e0e0e0', borderRadius: 16 },
-    card: { alignItems: 'center', marginBottom: 16 },
-    cardImage: { width: '20%', height: 65, borderRadius: 16, position: 'relative', top: -330 },
-    cardText: { position: 'absolute', color: 'white', fontSize: 20, fontWeight: 'bold', top: '40%' },
-    button: { backgroundColor: '#007bff', padding: 16, borderRadius: 8, alignItems: 'center' },
-    buttonText: { color: '#fff', fontSize: 18 },
-    subtext: { color: '#fff', fontSize: 12, marginTop: 4 },
-    footer: { flexDirection: 'row', justifyContent: 'space-around', paddingVertical: 16, borderTopWidth: 1, borderColor: '#ddd' }
+container: { 
+    flex: 1, 
+    padding: 16, 
+    backgroundColor: '#fff'
+},
+    header: { 
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: 'space-between', 
+        padding: 16 
+    },
+    headerTitle: { 
+        fontSize: 20, 
+        fontWeight: 'bold' 
+    },
+    profileImage: { 
+        width: 32, 
+        height: 32, 
+        borderRadius: 16 
+    },
+    searchFilterContainer: { 
+        flexDirection: 'row', 
+        justifyContent: 'space-between', 
+        marginVertical: 16 },
+    searchText: { 
+        fontSize: 24, 
+        fontWeight: 'bold' 
+    },
+    sortFilter: { 
+        flexDirection: 'row' 
+    },
+    icon: { 
+        marginHorizontal: 8 
+    },
+    categories: { 
+        flexDirection: 'row', 
+        marginBottom: 14, 
+        width: 150, 
+    },
+    category: { 
+        marginHorizontal: 8, 
+        padding: 8, 
+        backgroundColor: '#e0e0e0', 
+        borderRadius: 16 
+    },
+    card: { 
+        alignItems: 'center', 
+        marginBottom: 16 
+    },
+    cardImage: { 
+        width: '20%', 
+        height: 65, 
+        borderRadius: 16, 
+        position: 'relative', 
+        top: -330 
+    },
+    cardText: { 
+        position: 'absolute', 
+        color: 'white', 
+        fontSize: 20, 
+        fontWeight: 'bold', 
+        top: '40%' 
+    },
+    button: { 
+        backgroundColor: '#007bff', 
+        padding: 16, 
+        borderRadius: 8, 
+        alignItems: 'center' 
+    },
+    buttonText: { 
+        color: '#fff', 
+        fontSize: 18 
+    },
+    subtext: { 
+        color: '#fff', 
+        fontSize: 12, 
+        marginTop: 4 
+    },
+    footer: { 
+        flexDirection: 'row', 
+        justifyContent: 'space-around', 
+        paddingVertical: 16, 
+        borderTopWidth: 1, 
+        borderColor: '#ddd' 
+    }
 });
 
 export default HomeScreen;
