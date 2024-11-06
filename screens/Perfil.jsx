99f8f44cc2f6a99673a65ae859572251b6ca37dd
lucascov06 <scovasso.luca.epet20@gmail.com>
2024-11-05 21:12:50 -0300

    Haciendo sección detalles id académico

--- screens/Perfil.jsx
+++ screens/Perfil.jsx
@@ -45,3 +45,44 @@ const Perfil = ({ navigation }) => {
           </TouchableOpacity>
         </View>
 
+        <View style={styles.section}>
+          <Text style={styles.label}>Detalles de ID Académico</Text>
+          <TextInput
+            style={styles.input}
+            value={codigoId}
+            onChangeText={setCodigoId}
+            placeholder="Ingrese su Código ID"
+            placeholderTextColor="#8A8A8A"
+          />
+          <TextInput
+            style={styles.input}
+            value={domicilio}
+            onChangeText={setDomicilio}
+            placeholder="Ingrese su domicilio"
+            placeholderTextColor="#8A8A8A"
+          />
+
+          <Picker
+            selectedValue={ciudad}
+            style={[styles.picker, ciudad === '' && { color: '#8A8A8A' }] }
+            onValueChange={(itemValue) => setCiudad(itemValue)}
+          >
+            <Picker.Item label="Seleccione ciudad" value="" color="#8A8A8A" />
+            <Picker.Item label="Neuquén" value="Neuquén" />
+            <Picker.Item label="Cipolletti" value="Cipolletti" />
+            <Picker.Item label="Centenario" value="Centenario" />
+            <Picker.Item label="Plottier" value="Plottier" />
+          </Picker>
+
+          <Picker
+            selectedValue={pais}
+            style={[styles.picker, pais === '' && { color: '#8A8A8A' }] }
+            onValueChange={(itemValue) => setPais(itemValue)}
+          >
+            <Picker.Item label="Seleccione país" value="" color="#8A8A8A" />
+            <Picker.Item label="Argentina" value="Argentina" />
+            <Picker.Item label="Chile" value="Chile" />
+            <Picker.Item label="Brasil" value="Brasil" />
+          </Picker>
+        </View>
+
