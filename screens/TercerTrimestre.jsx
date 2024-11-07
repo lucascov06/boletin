5cc02cb33e6ea1d33c024c5fba831a8857fc78db
santy186 <vargassanti186@gmail.com>
2024-11-07 15:44:58 -0300

    Function de la pantalla

--- screens/TercerTrimestre.jsx
+++ screens/TercerTrimestre.jsx
@@ -2,3 +2,25 @@ import React from 'react';
 import { View, Text, Image, TouchableOpacity, ScrollView, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { useNavigation } from '@react-navigation/native';
+
+function Trimestre() {
+    const navigation = useNavigation(); 
+
+   
+    return (
+        <View style={styles.container}>
+           
+           <View style={styles.footer}>
+            <TouchableOpacity onPress={() => navigation.navigate('')}>
+                <Icon name="search" size={28} />
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
+                <Icon name="home" size={28} color="red" />
+            </TouchableOpacity>
+        </View>
+
+
+           
+            
+        </View>
