e80e8238970c95b97b9a2d87316f9bf1b402ba8d
santy186 <vargassanti186@gmail.com>
2024-11-07 15:44:18 -0300

    Imports de la pantalla TercerTrimestre

---
+++ screens/TercerTrimestre.jsx
@@ -0,0 +1,4 @@
+import React from 'react'; 
+import { View, Text, Image, TouchableOpacity, ScrollView, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
