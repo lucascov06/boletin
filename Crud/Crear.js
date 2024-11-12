8a637640a4b10d9fd4efcf229bf32fee208fd4df
obertti.thiago.epet20@gmail.com <Obertti.thiago.epet20@gmail.com>
2024-11-12 18:18:11 -0300

    mejorando crud

--- Crud/Crear.js
+++ Crud/Crear.js
@@ -1,14 +1,13 @@
-import { collection, addDoc } from "firebase/firestore";
-import { db } from "../firebaseConfig";
+import firestore from '@react-native-firebase/firestore';
 
-async function createDocument() {
-  try {
-    const docRef = await addDoc(collection(db, "Boletin"), {
-      campo1: "valor1",
-      campo2: "valor2",
-    });
-    console.log("Documento creado con ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error al crear el documento: ", e);
-  }
-}
+firestore().collection('usuarios').add({
+
+name: 'John Doe',
+
+age: 25,
+
+}).then(() => {
+
+console.log('Usuario Creado!');
+
+});
