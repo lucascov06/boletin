399533980a9416715b2247e09a15f16abe48b7cf
Emanuel <arielruizcontacto@gmail.com>
2024-11-12 19:27:42 -0300

    Enlazando pantallas

--- App.js
+++ App.js
@@ -12,7 +12,10 @@ import Crear from './screens/Crear.jsx';
 import HomeScreen from './screens/Home.jsx';
 import Perfil from './screens/Perfil.jsx';
 import TercerTrimestre from './screens/TercerTrimestre.jsx';
-import Materia from './screens/Materia.jsx'; // Asegúrate de que el archivo exista
+import Materia from './screens/Materia.jsx'; 
+import EmpezarProf from './screens/EmpezarProf.jsx'; 
+import HomeProf from './screens/HomeProf.jsx'; 
+
 
 const Stack = createStackNavigator();
 
@@ -30,6 +33,10 @@ const App = () => {
         <Stack.Screen name="Perfil" component={Perfil} />
         <Stack.Screen name="TercerTrimestre" component={TercerTrimestre} />
         <Stack.Screen name="Materia" component={Materia} />
+        <Stack.Screen name="EmpezarProf" component={EmpezarProf} />
+        <Stack.Screen name="HomeProf" component={HomeProf} />
+
+
       </Stack.Navigator>
     </NavigationContainer>
   );
