b7c2aa7581ef01cf9e6fec1f950d6d814b3c1c62
Emanuel <arielruizcontacto@gmail.com>
2024-11-12 16:29:08 -0300

    Implementando navegacion de materias

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-
 import React from 'react';
-import LoginScreen from '../boletin/screens/login.jsx';
-
-import Singin from './screens/Archivo.jsx';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+// Importación de pantallas
+import LoginScreen from '../boletin/screens/login.jsx';
 import Singinprofesor from './screens/singin2.jsx';
 import Empezar from './screens/Empezar.jsx';
 import Archivo from './screens/Archivo.jsx';
@@ -13,24 +12,27 @@ import Crear from './screens/Crear.jsx';
 import HomeScreen from './screens/Home.jsx';
 import Perfil from './screens/Perfil.jsx';
 import TercerTrimestre from './screens/TercerTrimestre.jsx';
+import Materia from './screens/Materia.jsx'; // Asegúrate de que el archivo exista
+
 const Stack = createStackNavigator();
+
 const App = () => {
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="Boletin Virtual">
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Archivo" component={Archivo} />
-      <Stack.Screen name="Crear" component={Crear} />
-      <Stack.Screen name="Singin" component={Singin} />
-      <Stack.Screen name="Contraseña" component={Contraseña} />
-      <Stack.Screen name="Empezar" component={Empezar} />
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
-      <Stack.Screen name="Singinprofesor" component={Singinprofesor} />
-      <Stack.Screen name="Perfil" component={Perfil} />
-      <Stack.Screen name="TercerTrimestre" component={TercerTrimestre} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Archivo" component={Archivo} />
+        <Stack.Screen name="Crear" component={Crear} />
+        <Stack.Screen name="Contraseña" component={Contraseña} />
+        <Stack.Screen name="Empezar" component={Empezar} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="Singinprofesor" component={Singinprofesor} />
+        <Stack.Screen name="Perfil" component={Perfil} />
+        <Stack.Screen name="TercerTrimestre" component={TercerTrimestre} />
+        <Stack.Screen name="Materia" component={Materia} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
--- navigation.js
+++ navigation.js
@@ -1,38 +1,40 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import Archivo from "./screens/Archivo";
 import { createStackNavigator } from '@react-navigation/stack';
-// screens
+
+// Screens
+import Archivo from "./screens/Archivo";
 import Singin from "./singin";
 import Singinprofesor from "./singin2";
 import Empezar from "./screens/Empezar";
-import Contraseña from "./screens/Contraseña"; 
-import Crear from "./screens/Crear"; 
+import Contraseña from "./screens/Contraseña";
+import Crear from "./screens/Crear";
 import HomeScreen from "./screens/Home";
 import Trimestre from "./screens/TercerTrimestre";
-
+import Materia from "./screens/Materia"; // Asegúrate de que el archivo sea correcto
 
 const Stack = createStackNavigator();
 
-function Mytabs() {
+function MyStack() {
     return (
         <Stack.Navigator>
-            <tab.Screen name="Archivo" component={Archivo} />
-            <tab.Screen name="Singin" component={Singin} />
-            <tab.Screen name="Empezar" component={Empezar} />
+            <Stack.Screen name="Archivo" component={Archivo} />
+            <Stack.Screen name="Singin" component={Singin} />
+            <Stack.Screen name="Empezar" component={Empezar} />
             <Stack.Screen name="HomeScreen" component={HomeScreen} />
-            <tab.Screen name="Crear" component={Crear} />
-            <tab.Screen name="Contraseña" component={Contraseña} />
-            <tab.Screen name="Singinprofesor" component={Singinprofesor} />
-            <tab.Screen name="TercerTrimestre" component={TercerTrimestre} />
+            <Stack.Screen name="Crear" component={Crear} />
+            <Stack.Screen name="Contraseña" component={Contraseña} />
+            <Stack.Screen name="Singinprofesor" component={Singinprofesor} />
+            <Stack.Screen name="TercerTrimestre" component={Trimestre} />
+            <Stack.Screen name="Materia" component={Materia} />
         </Stack.Navigator>    
     );
 }    
 
-export default function navigation() {
+export default function Navigation() {
     return (
         <NavigationContainer>
-            <Mytabs />
+            <MyStack />
         </NavigationContainer>
     );
 }
