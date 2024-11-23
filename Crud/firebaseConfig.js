3ff185e3e48b9783a78457a21bd47e25709cec3c
lucascov06 <scovasso.luca.epet20@gmail.com>
2024-11-23 14:32:26 -0300

    Configurando FirebaseConfig

--- Crud/firebaseConfig.js
+++ Crud/firebaseConfig.js
@@ -1,23 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD2mveUXmX8YZ093Dy1UPeQGna8JdDb9ec",
-    authDomain: "boletin-virtual.firebaseapp.com",
-    projectId: "boletin-virtual",
-    storageBucket: "boletin-virtual.appspot.com",
-    messagingSenderId: "107832750012",
-    appId: "1:107832750012:web:53d434cde9a73b915305b9",
-    measurementId: "G-4FHWJ2C5RF"
+  apiKey: "AIzaSyD2mveUXmX8YZ093Dy1UPeQGna8JdDb9ec",
+  authDomain: "boletin-virtual.firebaseapp.com",
+  projectId: "boletin-virtual",
+  storageBucket: "boletin-virtual.appspot.com",
+  messagingSenderId: "107832750012",
+  appId: "1:107832750012:web:53d434cde9a73b915305b9",
+  measurementId: "G-4FHWJ2C5RF"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Inicializar Firebase
+const appFirebase = initializeApp(firebaseConfig);
 
-export { db };
+// Inicializar Firestore
+export const db = getFirestore(appFirebase);
+
+
