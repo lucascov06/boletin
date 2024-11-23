b97f91d180a1773404bef22907b1a772882eeae0
obertti.thiago.epet20@gmail.com <Obertti.thiago.epet20@gmail.com>
2024-11-23 14:34:42 -0300

    Creando funcion de login

--- screens/Archivo.jsx
+++ screens/Archivo.jsx
@@ -1,97 +1,103 @@
 import React, { useState } from 'react';
-import { View, Text,Pressable, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';  
-import { useNavigation } from '@react-navigation/native';
 import Empezar from './Empezar';
+    import appFirebase from '../Crud/firebaseConfig';
+    import { getAuth,} from 'firebase/auth';
+    const auth = getAuth(appFirebase);
+    import { StyleSheet } from 'react-native';
+    import { View, Text, TextInput, TouchableOpacity, Image, Pressable, Alert } from 'react-native';
+    import { useNavigation } from '@react-navigation/native';
+    import { signInWithEmailAndPassword } from 'firebase/auth';
+    import Icon from 'react-native-vector-icons/FontAwesome';
 
-const Archivo = () => {
-  const navigation = useNavigation(); 
-    const [passwordVisible, setPasswordVisible] = useState(false);  
-   
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>¡Ingrese su Cuenta!</Text>
-
-            <TextInput
-                style={styles.input}
-                placeholder="Nombre de usuario o gmail"
-                placeholderTextColor="#8A8A8A"
-            />
-            
-            <TextInput
-                style={styles.input}
-                placeholder="Id boletín"
-                placeholderTextColor="#8A8A8A"
-            />
-            
-            <View style={styles.passwordContainer}>
+    const Archivo = (props) => {
+        const navigation = useNavigation();
+        const [password, setPassword] = useState('');
+        const [email, setEmail] = useState('');
+        const [showPassword, setShowPassword] = useState(false); // Estado para controlar la visualización de la contraseña
+    
+        const logueo = async () => {
+            try {
+                await signInWithEmailAndPassword(auth, email, password);
+                Alert.alert('Iniciando sesión', 'Iniciando....');
+                navigation.navigate('Empezar');
+            } catch (error) {
+                console.log(error);
+            }
+        };
+    
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>¡Ingrese su Cuenta!</Text>
+    
                 <TextInput
-                    style={styles.passwordInput}
-                    placeholder="Contraseña"
+                    style={styles.input}
+                    placeholder="Nombre de usuario o gmail"
                     placeholderTextColor="#8A8A8A"
-                    secureTextEntry={!passwordVisible}  
+                    onChangeText={(text) => setEmail(text)}
+                    value={email}
                 />
-                <TouchableOpacity onPress={() => setPasswordVisible(!passwordVisible)}>
-                    <Icon
-                        name={passwordVisible ? 'eye' : 'eye-slash'}  
-                        size={20}
-                        color="#8A8A8A"
+    
+                <View style={styles.passwordContainer}>
+                    <TextInput
+                        style={styles.passwordInput}
+                        placeholder="Contraseña"
+                        onChangeText={(text) => setPassword(text)}
+                        placeholderTextColor="#8A8A8A"
+                        secureTextEntry={!showPassword} 
+                        value={password}
                     />
-                </TouchableOpacity>
+                    <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                        <Icon
+                            name={showPassword ? 'eye' : 'eye-slash'}
+                            size={20}
+                            color="#8A8A8A"
+                        />
+                    </TouchableOpacity>
+                </View>
+    
+                <Pressable onPress={() => navigation.navigate('Contraseña')}>
+                    <Text style={styles.signUpLink}>¿Olvidaste tu contraseña?</Text>
+                </Pressable>
+    
+                <Pressable 
+                    style={styles.loginButton}
+                    onPress={logueo} // Ejecuta la función logueo
+                >
+                    <Text style={styles.loginButtonText}>Ingresar</Text>
+                </Pressable>
+    
+                <Text style={styles.orText}>- O continuar con -</Text>
+    
+                <View style={styles.socialButtons}>
+                    <TouchableOpacity>
+                        <Image
+                            style={styles.socialIcon}
+                            source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/640px-Google_%22G%22_logo.svg.png' }} 
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image
+                            style={styles.socialIcon}
+                            source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg' }} 
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image
+                            style={styles.socialIcon}
+                            source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg' }} 
+                        />
+                    </TouchableOpacity>
+                </View>
+    
+                <Pressable onPress={() => navigation.navigate('Crear')}>
+                    <Text style={styles.signUpText}>
+                        Crea una cuenta <Text style={styles.signUpLink}>Sign Up</Text>
+                    </Text>
+                </Pressable>
             </View>
-            
-            
-         
-
-      <Pressable >
-                <Text style={styles.signUpLinkt}onPress={() => navigation.navigate('Contraseña')}>¿Olvidaste tu contraseña?</Text>
-             </Pressable>
-            
-
-            
-            
-
-        
-
-
-             <Pressable 
-         style={styles.loginButton}onPress={() => navigation.navigate('Empezar')}>
-        <Text style={styles.loginButtonText}>Ingresar</Text>
-      </Pressable>
-
-            <Text style={styles.orText}>- O continuar con -</Text>
-
-            <View style={styles.socialButtons}>
-                <TouchableOpacity>
-                    <Image
-                        style={styles.socialIcon}
-                        source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/640px-Google_%22G%22_logo.svg.png' }} 
-                    />
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Image
-                        style={styles.socialIcon}
-                        source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg' }} 
-                    />
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Image
-                        style={styles.socialIcon}
-                        source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg' }} 
-                    />
-                </TouchableOpacity>
-            </View>
-
-            <Pressable >
-                <Text style={styles.signUpText}onPress={() => navigation.navigate('Crear')}>Crea una cuenta <Text style={styles.signUpLink}>Sign Up</Text></Text>
-            </Pressable>
-            
-        
-        
-        </View>
-    );
-};
-
+        );
+    };
+    
 const styles = StyleSheet.create({
     container: {
         flex: 1,
